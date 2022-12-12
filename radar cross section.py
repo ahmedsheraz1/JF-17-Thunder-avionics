d5f1678231e8f18470f13505e7192945723c9994
Ahmed Sheraz <ahmedsheraz1@gmail.com>
2022-12-12 20:28:52 +0500

    Create radar cross section.py

---
+++ radar cross section.py
@@ -0,0 +1,16 @@
+Python 3.10.2 (v3.10.2:a58ebcc701, Jan 13 2022, 14:50:16) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+list(range(10))
+[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+list(range(1, 11))
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+list(range(0, 30, 5))
+[0, 5, 10, 15, 20, 25]
+list(range(0, 10, 3))
+[0, 3, 6, 9]
+list(range(0, -10, -1))
+[0, -1, -2, -3, -4, -5, -6, -7, -8, -9]
+list(range(0))
+[]
+list(range(1, 0))
+[]
