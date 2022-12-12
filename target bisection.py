302fee5060543fc09a3fe29b0ed1c964b5519a21
Ahmed Sheraz <ahmedsheraz1@gmail.com>
2022-12-12 16:55:01 +0500

    Create target bisection.py

---
+++ target bisection.py
@@ -0,0 +1,7 @@
+Python 3.10.2 (v3.10.2:a58ebcc701, Jan 13 2022, 14:50:16) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+import bisect
+scores = [(100, 'perl'), (200, 'tcl'), (400, 'lua'), (500, 'python')]
+bisect.insort(scores, (300, 'ruby'))
+scores
+[(100, 'perl'), (200, 'tcl'), (300, 'ruby'), (400, 'lua'), (500, 'python')]
